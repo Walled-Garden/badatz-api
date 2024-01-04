5166723f53eeae9646726ecdb15a967374e56ffb
tmax22 <el96m8ro@p12test.onmicrosoft.com>
2024-01-04 12:47:09 +0200

    badatz-api

--- typescript-api/app/src/run-server.ts
+++ typescript-api/app/src/run-server.ts
@@ -1,11 +1,11 @@
 import { app } from "./server.js";
 
-app.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
+app.listen({ port: 5000, host: "0.0.0.0" }, (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`
-  🚀 Server ready at: http://localhost:3000
+  🚀 Server ready at: http://localhost:5000
   ⭐️ See sample requests: http://pris.ly/e/ts/rest-fastify#3-using-the-rest-api`);
 });
